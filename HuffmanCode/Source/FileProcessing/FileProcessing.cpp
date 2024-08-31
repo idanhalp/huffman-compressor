cb3da802cbf65a734cca281b34f92fdc2939b4a9
idanhalp <idanhal27@gmail.com>
2024-08-31 21:32:20 +0300

    Feature: Can encode to file and decode it back.

--- HuffmanCode/Source/FileProcessing/FileProcessing.cpp
+++ HuffmanCode/Source/FileProcessing/FileProcessing.cpp
@@ -4,22 +4,83 @@
 
 using namespace std;
 
-auto Processing::read_file(string input_file_path) -> string
+namespace Processing::Auxiliary
 {
-	ifstream input_file(input_file_path);
+	auto write_frequency_map_to_output_file(ofstream& output_stream, const Algorithm::frequency_map_t& frequency_map) -> void;
+	auto write_characters_to_output_file(ofstream& output_stream, const vector<char> characters) -> void;
+}
 
-	if (!input_file.is_open())
+auto Processing::read_file(string input_file_path) -> Algorithm::EncodingInfo
+{
+	ifstream input_stream(input_file_path);
+
+	if (!input_stream.is_open())
 	{
 		throw runtime_error("Cannot open " + input_file_path + "!\n");
 	}
 
-	string line;
-	string result;
+	Algorithm::EncodingInfo encoding_info;
+	string s;
 
-	while (getline(input_file, line))
+	getline(input_stream, s);
+	encoding_info.total_bits = stoi(s);
+
+	getline(input_stream, s);
+	const int frequency_map_length = stoi(s);
+
+	for (int i = 0; i < frequency_map_length; ++i)
 	{
-		result += line;
+		char character;
+		int frequency;
+
+		input_stream >> character >> frequency;
+		encoding_info.frequency_map[character] = frequency;
 	}
 
-	return result;
+	getline(input_stream, s);
+	getline(input_stream, s);
+	const int num_of_characters = stoi(s);
+
+	for (int i = 0; i < num_of_characters; ++i)
+	{
+		char character;
+		input_stream.get(character);
+		encoding_info.encoded_characters.push_back(character);
+	}
+
+	return encoding_info;
+}
+
+auto Processing::write_file(std::string output_file_path, const Algorithm::EncodingInfo& encoding_info) -> void
+{
+	ofstream output_stream(output_file_path);
+
+	if (!output_stream.is_open())
+	{
+		throw runtime_error("Cannot write to " + output_file_path + "!\n");
+	}
+
+	output_stream << encoding_info.total_bits << '\n';
+	Auxiliary::write_frequency_map_to_output_file(output_stream, encoding_info.frequency_map);
+	Auxiliary::write_characters_to_output_file(output_stream, encoding_info.encoded_characters);
+}
+
+auto Processing::Auxiliary::write_frequency_map_to_output_file(ofstream& output_stream, const Algorithm::frequency_map_t& frequency_map) -> void
+{
+	output_stream << frequency_map.size() << '\n';
+	
+	for (const auto [character, frequency] : frequency_map)
+	{
+		output_stream << character << ' ' << frequency << '\n';
+	}
+}
+
+auto Processing::Auxiliary::write_characters_to_output_file(ofstream& output_stream, const vector<char> characters) -> void
+{
+	output_stream << characters.size() << '\n';
+
+	for (const char character : characters)
+	{
+		output_stream << character;
+	}
 }
--- HuffmanCode/Source/FileProcessing/FileProcessing.hpp
+++ HuffmanCode/Source/FileProcessing/FileProcessing.hpp
@@ -1,13 +1,13 @@
 #ifndef FILE_PROCESSING_HPP
 #define FILE_PROCESSING_HPP
 
+#include "../Algorithm/Algorithm.hpp"
 #include <string>
-#include <string_view>
 
 namespace Processing
 {
-	auto read_file(std::string input_file_path) -> std::string;
-	auto write_file(std::string_view content, std::string output_file_path) -> void;
+	auto read_file(std::string input_file_path) -> Algorithm::EncodingInfo;
+	auto write_file(std::string output_file_path, const Algorithm::EncodingInfo& encoding_info) -> void;
 }
 
 #endif // FILE_PROCESSING_HPP
