e077bcaa3aabf2a776e434ac1fba55f20470b63c
idanhalp <idanhal27@gmail.com>
2024-09-01 18:06:59 +0300

    Reordered `Processing` functions.

--- HuffmanCode/Source/FileProcessing/FileProcessing.hpp
+++ HuffmanCode/Source/FileProcessing/FileProcessing.hpp
@@ -6,8 +6,8 @@
 
 namespace Processing
 {
-	auto decode_from_file(std::string input_file_path) -> Algorithm::EncodingInfo;
 	auto encode_into_file(std::string output_file_path, const Algorithm::EncodingInfo& encoding_info) -> void;
+	auto decode_from_file(std::string input_file_path) -> Algorithm::EncodingInfo;
 }
 
 #endif // FILE_PROCESSING_HPP
